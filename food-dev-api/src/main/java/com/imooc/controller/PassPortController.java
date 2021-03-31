e4a67ccd9ac470edf779959462eb7a877d0bdbc0
kyh5266 <1412053341@qq.com>
2021-03-31 17:01:56 +0800

    4-7

--- food-dev-api/src/main/java/com/imooc/controller/PassPortController.java
+++ food-dev-api/src/main/java/com/imooc/controller/PassPortController.java
@@ -3,10 +3,13 @@ package com.imooc.controller;
 import com.imooc.pojo.bo.UserBO;
 import com.imooc.service.UserService;
 import com.imooc.utils.IMOOCJSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Api(value = "注册登录", tags = {"用于注册登录的相关接口"})
 @RestController
 @RequestMapping("passport")
 public class PassPortController {
@@ -14,6 +17,7 @@ public class PassPortController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "用户名是否存在", notes = "用户名是否存在", httpMethod = "GET")
     @GetMapping("/usernameIsExist")
     public IMOOCJSONResult usernameIsExist(@RequestParam String username) {
         //1.判断用户名是否为空
@@ -31,6 +35,7 @@ public class PassPortController {
         return IMOOCJSONResult.ok();
     }
 
+    @ApiOperation(value = "用户注册", notes = "用户注册", httpMethod = "POST")
     @PostMapping("/regist")
     public IMOOCJSONResult regist(@RequestBody UserBO userBO) {
         String username = userBO.getUsername();
