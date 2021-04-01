3f08c86c5a961527da50e4359e663623cdbed304
kyh5266 <1412053341@qq.com>
2021-04-01 17:00:55 +0800

    4-7

---
+++ food-dev-api/src/main/java/com/imooc/config/CrosConfig.java
@@ -0,0 +1,33 @@
+package com.imooc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CrosConfig {
+    public CrosConfig() {
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        //1.添加cors配置信息
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhots:8080");
+
+        //设置是否发送cookie信息
+        corsConfiguration.setAllowCredentials(true);
+
+        //设置请求方式
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+
+        //2.为URL添加映射路径
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(corsConfigurationSource);
+    }
+}
