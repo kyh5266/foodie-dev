2a8292a1d26843fa0112498a90381aa723830b14
kyh5266 <1412053341@qq.com>
2021-05-14 16:51:05 +0800

    5/14

--- food-dev-service/src/main/java/com/imooc/service/impl/ItemServiceImpl.java
+++ food-dev-service/src/main/java/com/imooc/service/impl/ItemServiceImpl.java
@@ -1,21 +1,24 @@
 package com.imooc.service.impl;
 
-import com.imooc.mapper.ItemsImgMapper;
-import com.imooc.mapper.ItemsMapper;
-import com.imooc.mapper.ItemsParamMapper;
-import com.imooc.mapper.ItemsSpecMapper;
-import com.imooc.pojo.Items;
-import com.imooc.pojo.ItemsImg;
-import com.imooc.pojo.ItemsParam;
-import com.imooc.pojo.ItemsSpec;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.imooc.enums.CommentLevel;
+import com.imooc.mapper.*;
+import com.imooc.pojo.*;
+import com.imooc.pojo.vo.CommentLevelCountsVO;
+import com.imooc.pojo.vo.ItemCommentVO;
 import com.imooc.service.ItemService;
+import com.imooc.utils.DesensitizationUtil;
+import com.imooc.utils.PagedGridResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -31,6 +34,12 @@ public class ItemServiceImpl implements ItemService {
 
     @Autowired
     private ItemsParamMapper itemsParamMapper;
+
+    @Autowired
+    private ItemsCommentsMapper itemsCommentsMapper;
+
+    @Autowired
+    private ItemsMapperCustom itemsMapperCustom;
 
     /**
      * 根据商品ID查询详情
@@ -88,4 +97,70 @@ public class ItemServiceImpl implements ItemService {
         criteria.andEqualTo("itemId", itemId);
         return itemsParamMapper.selectOneByExample(itemParamExp);
     }
+
+    /**
+     * 根据商品ID查询商品的评价等级数量
+     *
+     * @param itemId
+     * @return
+     */
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public CommentLevelCountsVO queryCommentCounts(String itemId) {
+        Integer goodConuts = getCommentCounts(itemId, CommentLevel.GOOD.type);
+        Integer badConuts = getCommentCounts(itemId, CommentLevel.BAD.type);
+        Integer normalConuts = getCommentCounts(itemId, CommentLevel.NORMAL.type);
+        Integer totalCounts = goodConuts + badConuts + normalConuts;
+
+        CommentLevelCountsVO commentLevelCountsVO = new CommentLevelCountsVO();
+        commentLevelCountsVO.setBadCounts(badConuts);
+        commentLevelCountsVO.setGoodCounts(goodConuts);
+        commentLevelCountsVO.setNormalCounts(normalConuts);
+        commentLevelCountsVO.setTotalCounts(totalCounts);
+        return commentLevelCountsVO;
+    }
+
+    /**
+     * 根据商品ID查询商品的评价（分页）
+     *
+     * @param itemId
+     * @param level
+     * @return
+     */
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public PagedGridResult queryPagedComments(String itemId, Integer level, Integer page, Integer pageSize) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("itemId", itemId);
+        map.put("level", level);
+
+        PageHelper.startPage(page, pageSize);
+        List<ItemCommentVO> list = itemsMapperCustom.queryItemComments(map);
+        //昵称脱敏处理
+        for (ItemCommentVO vo: list) {
+            vo.setNickname(DesensitizationUtil.commonDisplay(vo.getNickname()));
+        }
+        return setterPagedGrid(list, page);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    Integer getCommentCounts(String itemId, Integer level) {
+        ItemsComments itemsComments = new ItemsComments();
+        itemsComments.setItemId(itemId);
+        if (level != null) {
+            itemsComments.setCommentLevel(level);
+        }
+        return itemsCommentsMapper.selectCount(itemsComments);
+    }
+
+    private PagedGridResult setterPagedGrid(List<?> list, Integer page) {
+        PageInfo<?> pageList = new PageInfo<>(list);
+        PagedGridResult grid = new PagedGridResult();
+        grid.setPage(page);
+        grid.setRows(list);
+        grid.setTotal(pageList.getPages());
+        grid.setRecords(pageList.getTotal());
+        return grid;
+    }
+
 }
